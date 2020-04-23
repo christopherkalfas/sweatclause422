e98bb8b2332ffc789fbf101fa8e3db63143470f5
Chris Kalfas <christopherkalfas22@gmail.com>
2020-04-23 12:45:38 -0400

    added views for all models. Working on challenge show tracker table.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+    def current_user
+        user_id = session[:user_id]
+        # byebug
+        user_id && User.find(user_id)
+      end
+    
+      def authenticate!
+        unless current_user
+          flash[:info] = "You have to be logged in to do that!"
+          redirect_to login_path
+        end
+      end
 end
