c12446e0762e561e60dabec866b8f7da6ff6b3f8
Chris Kalfas <christopherkalfas22@gmail.com>
2020-04-23 12:07:48 -0400

    Added Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'welcome#index'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destory'
+  
+  get '/users/:id/trackers', to: 'users#trackers'
+  resources :users
+  resources :groups
+  resources :challenges
+  resources :trackers
 end
