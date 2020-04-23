0e9f3c4f037e1b31e5d2af9fd1015187d1b76087
Chris Kalfas <christopherkalfas22@gmail.com>
2020-04-23 17:02:12 -0400

    Added a column for updating each tracker. Should try to figure out how to make it so other users can not edit another users tracker.

--- app/models/tracker.rb
+++ app/models/tracker.rb
@@ -1,4 +1,8 @@
 class Tracker < ApplicationRecord
     belongs_to :user
     belongs_to :challenge
+
+    def total_reps
+        total = self.sunday_reps + self.monday_reps + self.tuesday_reps + self.wednesday_reps + self.thursday_reps + self.friday_reps + self.saturday_reps
+    end 
 end
