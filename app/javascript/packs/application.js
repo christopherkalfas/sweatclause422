e7c0d1342d3fb53b1f44a1d7300ab2c7ed571f91
Chris Kalfas <christopherkalfas22@gmail.com>
2020-05-21 14:28:24 -0400

    Pretty sure I fixed the webpacker issue. But this is alos the third time I think I've fixed it. So we will see...Moving on

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,13 +7,17 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("jquery")
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
 
 import 'bootstrap'
-import ".../assets/stylesheets/application.scss"
+import "../stylesheets/application.scss"
 
 document.addEventListener("turbolinks:load", ()=> {
     $('[data-toggle="tooltip"]').tooltip()
-    $('[data-toggle="tooltip"]').popover()
+    $('[data-toggle="popover"]').popover()
 })
 
 
