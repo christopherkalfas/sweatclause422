e64aa58572452641f9f4992dd7c25d1acd4cf736
Chris Kalfas <christopherkalfas22@gmail.com>
2020-06-27 16:56:07 -0400

    tried to set up an app two-step ver thru gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ Rails.application.configure do
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
-  # config.require_master_key = true
+  config.require_master_key = true
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
@@ -60,23 +60,23 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "sweatclause422_production"
 
-  # config.action_mailer.perform_caching = false
-  # config.action_mailer.default_url_options = { host: "radiant-wave-27873.herokuapp.com", protocol: "http" }
+  config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: "radiant-wave-27873.herokuapp.com", protocol: "http" }
 
-  # config.action_mailer.raise_delivery_errors = true
-  # config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
  
   
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.smtp_settings = {
-  # address: "smtp.gmail.com",
-  # port: 587,
-  # domain: "gmail.com",
-  # user_name: ENV["GMAIL_USERNAME"],
-  # password: ENV["GMAIL_PASSWORD"],
-  # authentication: "plain",
-  # enable_starttls_auto: true
-# }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: "gmail.com",
+  user_name: ENV["GMAIL_USERNAME"],
+  password: ENV["GMAIL_PASSWORD"],
+  authentication: "plain",
+  enable_starttls_auto: true
+}
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
