c94fdd12598a47d3178f1764469be64e7e5e072d
Chris Kalfas <christopherkalfas22@gmail.com>
2020-06-27 17:50:58 -0400

    tried a different format for mailer to log into gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,6 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
   address: "smtp.gmail.com",
   port: 587,
-  domain: "gmail.com",
   user_name: ENV["GMAIL_USERNAME"],
   password: ENV["GMAIL_PASSWORD"],
   authentication: "plain",
