724412f229c8141c3b2f1b7285f51af4d0b9f69e
Chris Kalfas <christopherkalfas22@gmail.com>
2020-06-27 15:08:06 -0400

    remove unused migration files

--- db/migrate/20200622182252_remove_owner_id_from_groups.rb
+++
@@ -1,5 +0,0 @@
-class RemoveOwnerIdFromGroups < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :groups, :owner_id, :integer
-  end
-end
--- db/migrate/20200627170033_drop_invites_drop_memberships.rb
+++
@@ -1,6 +0,0 @@
-class DropInvitesDropMemberships < ActiveRecord::Migration[6.0]
-  def change
-    drop_table :memberships
-    drop_table :invites
-  end
-end
