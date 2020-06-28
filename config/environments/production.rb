5d8627f71423b70e83b7d3ea385650ce2f6938fb
Chris Kalfas <christopherkalfas22@gmail.com>
2020-06-28 16:09:02 -0400

    trying to get mailer to send still. tried another way to format creds

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "sweatclause422_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: "sweatclause.com", protocol: "http" }
+  config.action_mailer.default_url_options = { host: "radiant-wave-27873.herokuapp.com"}
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
@@ -69,12 +69,12 @@ Rails.application.configure do
   
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-  address: "smtp.gmail.com",
-  port: 587,
-  user_name: ENV["GMAIL_USERNAME"],
-  password: ENV["GMAIL_PASSWORD"],
-  authentication: "plain",
-  enable_starttls_auto: true
+    port: 587,
+    address: "smtp.gmail.com",
+    user_name: ENV["SMTP_USER_NAME"],
+    password: ENV["SMTP_PASSWORD"],
+    authentication: "plain",
+    enable_starttls_auto: true
 }
 
   # Ignore bad email addresses and do not raise email delivery errors.
