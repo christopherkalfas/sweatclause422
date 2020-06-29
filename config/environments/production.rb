5b6314423f81a23182e0c83549555423fb2e126e
Chris Kalfas <christopherkalfas22@gmail.com>
2020-06-29 17:32:16 -0400

    trying another config2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     port: 587,
     address: "smtp.gmail.com",
-    domain: "gmail.com"
+    domain: "gmail.com",
     user_name: ENV["GMAIL_USERNAME"],
     password: ENV["GMAIL_PASSWORD"],
     authentication: "login",
