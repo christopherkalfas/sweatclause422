7c7a17638e2eece1f1e9dd6efc2ab2fd7864c5bd
Chris Kalfas <christopherkalfas22@gmail.com>
2020-07-28 12:57:18 -0400

    add external charity links to group page and styled more forms

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,7 @@
 class Group < ApplicationRecord
     has_many :challenges
     has_many :memberships
+    has_many :charities, through: :challenges
     has_many :users, through: :memberships
     belongs_to :owner, class_name: 'User', foreign_key: :owner_id
 
