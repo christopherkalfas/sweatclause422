c065588956d67cce7d281a6b6f242f82ebedac46
christopherkalfas <christopherkalfas22@gmail.com>
2020-08-11 16:12:52 -0400

    first commit from new computer. also added pledge show page

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -17,7 +17,7 @@ class PasswordResetsController < ApplicationController
     if @user.password_reset_sent_at < 2.hours.ago
       redirect_to new_password_reset_path, :alert => "Password reset has expired."
     elsif @user.update(params.permit![:user])
-      redirect_to root_url, :notice => "Password has been reset!"
+      redirect_to root_url, :success => "Password has been reset!"
     else
       render :edit
     end
